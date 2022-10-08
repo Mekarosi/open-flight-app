0ba1c0451970606907fa104fcd9a68cd74166ca8
Mekarosi <mekarosi_assa@yahoo.com>
2022-10-08 10:27:00 -0700

    Added airlines state and installed axios

--- app/javascript/controllers/Airlines.js
+++ app/javascript/controllers/Airlines.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Airlines = () => {
     return (
-        <h1>This is the Airlines#index view for our app</h1>
+        <h1>This is the Airline#index view for our app</h1>
     )
     
 }
