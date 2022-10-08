2485bb943c505a9313ad32bb7dc67111561003f2
Mekarosi <mekarosi_assa@yahoo.com>
2022-10-08 10:57:53 -0700

    Used axios to fetch airlines

--- app/javascript/controllers/Airlines.js
+++ app/javascript/controllers/Airlines.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
 
 const Airlines = () => {
+    const [airlines, setAirlines] = useState([])
+
+    useEffect(() => {
+      // Get all of our airlines from api
+      // Update airlines in our state
+      axios.get('api/v1/airlines.json')
+      .then(resp => {
+        setAirlines(resp.data.data)
+      })
+      .catch(resp => console.log(resp))
+      
+    }, [airlines.length])
+    
+    const list = airlines.map(item => {
+        return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+    })
+
+
+
     return (
-        <h1>This is the Airline#index view for our app</h1>
+        <Fragment>
+           <h1>This is the Airline#index view for our app</h1>
+           <ul>{list}</ul>
+        </Fragment>
+        
     )
     
 }
